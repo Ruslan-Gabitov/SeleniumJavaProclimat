01808b6282d01de2ef9a21ec6d83235248eee0ad
Ruslan Gabitov <rgabitov355@gmail.com>
2024-09-26 18:12:28 +0500

    Add constructor BasePage and function openPage

--- src/main/java/pages/base/BasePage.java
+++ src/main/java/pages/base/BasePage.java
@@ -1,4 +1,18 @@
 package pages.base;
 
+import org.openqa.selenium.WebDriver;
+
 public class BasePage {
+
+    protected WebDriver driver;
+
+    public BasePage(WebDriver driver) {
+        this.driver = driver;
+    }
+
+    public void openPage (String url){
+        driver.get(url);
+    }
+
+
 }
