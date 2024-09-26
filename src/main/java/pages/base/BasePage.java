882071dd6fb105b7836f62e519de0edc4aacd058
Ruslan Gabitov <rgabitov355@gmail.com>
2024-09-26 18:54:59 +0500

    Added wait visible element function to BasePage

--- src/main/java/pages/base/BasePage.java
+++ src/main/java/pages/base/BasePage.java
@@ -1,6 +1,11 @@
 package pages.base;
 
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+
+import static common.Config.EXPLICIT_WAIT;
 
 public class BasePage {
 
@@ -12,6 +17,11 @@ public class BasePage {
 
     public void openPage (String url){
         driver.get(url);
+    }
+
+    public WebElement waitElementIsVisible(WebElement element){
+        new WebDriverWait(driver, EXPLICIT_WAIT).until(ExpectedConditions.visibilityOf(element));
+        return element;
     }
 
 
