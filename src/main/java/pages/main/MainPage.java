5d4a3da367bee246a7081c308704d738123401c4
Ruslan Gabitov <rgabitov355@gmail.com>
2024-09-26 22:23:57 +0500

    Added locators MainPage

--- src/main/java/pages/main/MainPage.java
+++ src/main/java/pages/main/MainPage.java
@@ -10,5 +10,9 @@ public class MainPage extends BasePage {
         super(driver);
     }
 
+    private final By MY_OFFICE = By.xpath(".//div[@class='personal_wrap']//a");
+    private final By USER_LOGIN_POPUP = By.xpath(".//input[@id='USER_LOGIN_POPUP']");
+    private final By USER_PASSWORD_POPUP = By.xpath(".//input[@id='USER_PASSWORD_POPUP']");
+    private final By REGISTRATION_BUTTON = By.xpath(".//div[@class='inner-table-block']//a");
 
 }
