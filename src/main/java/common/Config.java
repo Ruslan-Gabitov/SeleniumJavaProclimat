3af492ce93c699d1e54bc38b3686f74d639ffefc
Ruslan Gabitov <rgabitov355@gmail.com>
2024-09-26 18:57:59 +0500

    Added explisit wait constant to Config

--- src/main/java/common/Config.java
+++ src/main/java/common/Config.java
@@ -1,11 +1,12 @@
 package common;
 
 import java.time.Duration;
+
 import org.openqa.selenium.PageLoadStrategy;
 
 public class Config {
     public static final Duration IMPLICIT_WAIT = Duration.ofSeconds(10L);
     public static final PageLoadStrategy PAGE_LOAD_STRATEGY = PageLoadStrategy.EAGER;
-
+    public static final Duration EXPLICIT_WAIT = Duration.ofSeconds(10L);
 
 }
