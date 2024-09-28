2b06f361eba38c8c4b282ac60db48beeb5e64bf9
Ruslan Gabitov <rgabitov355@gmail.com>
2024-09-28 23:26:15 +0500

    Added function getRandomString to created random email

--- src/main/java/pages/registration/FakeDataGeneration.java
+++ src/main/java/pages/registration/FakeDataGeneration.java
@@ -1,2 +1,20 @@
-package pages.registration;public class FakeDataGeneration {
+package pages.registration;
+
+import java.util.Random;
+
+public class FakeDataGeneration {
+    private static String getRandomString() {
+        String SALTCHARS = "abcdefghijklmnopqrstuvwxyzZ1234567890";
+        StringBuilder salt = new StringBuilder();
+        Random rnd = new Random();
+        while (salt.length() < 10) { // length of the random string.
+            int index = (int) (rnd.nextFloat() * SALTCHARS.length());
+            salt.append(SALTCHARS.charAt(index));
+        }
+        return salt.toString();
+
+
+    }
+
+
 }
