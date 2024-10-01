5fc21e31e0d75b42b0e9a4cc86e8f5674ffce9b3
Ruslan Gabitov <rgabitov355@gmail.com>
2024-10-01 23:30:18 +0500

    Added constants for locators of registration form fields

--- src/main/java/pages/registration/RegistrationPage.java
+++ src/main/java/pages/registration/RegistrationPage.java
@@ -1,5 +1,6 @@
 package pages.registration;
 
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import pages.base.BasePage;
 
@@ -8,6 +9,11 @@ public class RegistrationPage extends BasePage {
     public RegistrationPage(WebDriver driver) {
         super(driver);
     }
+    private final By LOCATOR_INPUT_NAME = By.xpath(".//input[@id='input_NAME']");
+    private final By LOCATOR_INPUT_EMAIL = By.xpath(".//input[@id='input_EMAIL']");
+    private final By LOCATOR_INPUT_PHONE = By.xpath(".//input[@id='input_PERSONAL_PHONE']");
+    private final By LOCATOR_INPUT_PASSWORD = By.xpath(".//input[@id='input_PASSWORD']");
+    private final By LOCATOR_INPUT_CONFIRM_PASSWORD = By.xpath(".//input[@id='input_CONFIRM_PASSWORD']");
 
 
 }
